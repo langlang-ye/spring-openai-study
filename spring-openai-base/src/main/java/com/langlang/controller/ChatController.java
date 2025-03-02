99f02c82f3c8395e4e6e79a4b6f55f005f695533
langlang-ye <langlang_sir@163.com>
2025-03-02 23:55:48 +0800

    -- 切换模型测试生成图片

--- spring-openai-base/src/main/java/com/langlang/controller/ChatController.java
+++ spring-openai-base/src/main/java/com/langlang/controller/ChatController.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@RestController
+// @RestController
 public class ChatController {
 
     @Resource
---
+++ spring-openai-base/src/main/java/com/langlang/controller/ImageController.java
@@ -0,0 +1,42 @@
+package com.langlang.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 生成图片
+ */
+@RestController
+public class ImageController {
+
+    @Resource
+    private OpenAiImageModel openAiImageModel;
+
+    @RequestMapping("/ai/image")
+    private Object image(@RequestParam(value = "message") String msg) {
+        System.out.println(openAiImageModel);
+        ImageResponse imageResponse = openAiImageModel.call(new ImagePrompt(msg, OpenAiImageOptions.builder()
+                .withQuality("hd")
+                .withN(2) // 可以一次生成多张图片, dall-e-3 每次只能生成一张
+                .withHeight(1024)
+                .withWidth(1024)
+                .build()));
+
+        System.out.println(imageResponse);
+        String url = imageResponse.getResult().getOutput().getUrl();
+        // 对图片的地址进行处理
+
+        return imageResponse;
+    }
+
+
+
+
+
+}
